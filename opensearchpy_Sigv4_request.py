4e6a14c569d49a6c44bc2fc4efadf677aa5047eb
Christopher Sharkey <ev2900@gmail.com>
2022-12-19 17:28:05 -0600

    updates

---
+++ opensearchpy_Sigv4_request.py
@@ -0,0 +1,36 @@
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth # pip install opensearch-py
+import boto3
+
+host = 'search-opensearch-demo-basic-auth-tf2ubrkbsq63l3fjpycipkd364.us-east-1.es.amazonaws.com' # ex. search-opensearch-demo-basic-auth-tf2ubrkbsq63l3fjpycipkd364.us-east-1.es.amazonaws.com
+region = 'us-east-1'
+
+credentials = boto3.Session(profile_name='profile-2').get_credentials()
+
+auth = AWSV4SignerAuth(credentials, region)
+
+index_name = 'movies'
+
+client = OpenSearch(
+    hosts = [{'host': host, 'port': 443}],
+    http_auth = auth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
+
+# Add a document to the index.
+document = {
+  'title': 'Moneyball',
+  'director': 'Bennett Miller',
+  'year': '2011'
+}
+id = '1'
+
+response = client.index(
+    index = index_name,
+    body = document,
+    id = id,
+    refresh = True
+)
+
+print(response)
